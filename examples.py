2cd745ab90e8ab4821e8dcb6209582f0bbdab739
garimagattni123456789 <54619086+garimagattni123456789@users.noreply.github.com>
2021-08-27 00:27:13 -0700

    Add files via upload

---
+++ examples.py
@@ -0,0 +1,74 @@
+  
+'''#WAP for area of circle pi*r*r
+pi=3.14
+r=5
+A=pi*r*r
+print("Area of circle",A)'''
+
+'''#WAP for area of retangle l*b
+l=4
+b=2
+A=l*b
+print("Area of rectangle",A)'''
+
+'''WAP for percentage of five books pi+p2+p3+p4+p5/100
+p1=10
+p2=20
+p3=30
+p4=40
+p5=50
+total=p1+p2+p3+p4+p5
+P=p1+p2+p3+p4+p5/5.0
+print("Percentage of five books",P)'''
+
+'''#WAP for area of retangle l*b
+l=4
+b=2
+A=l*b
+print("Area of rectangle",A)'''
+
+'''#WAP for perimeter of retangle l+b
+l=4
+b=2
+P=l+b
+print("Perimeter of rectangle",P)'''
+
+'''#WAP to print 2 table
+num=2
+# to take input from user
+#num = int(input("dispplay multiplication table of"))
+#iterate 10 times from i=1 to 10
+for i in range(1,11):
+    print(num,'x',i,'=',num*i)'''
+
+'''#sum of 1si 10 natural numbers
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num3=int(input("enter the third number:"))
+num4=int(input("enter the fourth number:"))
+num5=int(input("enter the fifth number:"))
+num6=int(input("enter the sixth number:"))
+num7=int(input("enter the seventh number:"))
+num8=int(input("enter the eighth number:"))
+num9=int(input("enter the nineth number:"))
+num10=int(input("enter the tenth number:"))
+sum= int(num1)+ int(num2)+ int(num3)+ int(num4)+ int(num5)+int(num6)+ int(num7)+ int(num8)+ int(num9)+ int(num10)
+print(sum)'''
+
+'''#add,mul,div,sub
+#WAP for swapping of two numbers using third variable
+a=10
+b=5
+print("before swapping  A=",a," ","B=",b)
+temp=a
+a=b
+b=temp
+print("After swapping  A=",a," ","B=",b)'''
+
+'''#add,mul,div,sub
+#WAP for swapping of two numbers without using third variable
+p=int(input("Enter principle amount"))
+r=int(input("Enter rate of interest"))
+t=int(input("Enter duration of loan amaount"))
+si=p*r*t/100
+print("simple interest=",si)'''
---
+++ indentifiers.py
@@ -0,0 +1,29 @@
+'''a=15
+b=2
+print(id(a))
+print(id(b))
+print(a is not b)'''
+
+'''a=15
+b=2
+print(id(a))
+print(id(b))
+print(a is b)'''
+
+'''a=30
+b=30
+print(id(a))
+print(id(b))
+print(a is b)'''
+
+'''a=30
+b=30
+print(id(a))
+print(id(b))
+print(a is not b)'''
+
+'''a="help4code"
+print('e' not in a)'''
+
+'''a="help4code"
+print('e' in a)'''
